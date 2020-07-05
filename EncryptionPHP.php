d62094ff34732bcc3044ba82b606b544ff32b6f3
Miguel Pérez <miguelpercham@gmail.com>
2020-07-05 13:11:09 -0500

    Se completa la funcion encryptDB

--- EncryptionPHP.php
+++ EncryptionPHP.php
@@ -2,20 +2,45 @@
 
 class EncryptionPHP
 {
+    /**
+     * Almacena la llave de encriptacion
+     * @var string
+     */
     private $key;
+    /**
+     * Almacena el nombre de la tabla
+     * @var string
+     */
     private $tableName;
+    /**
+     * Almacena los nombres de las columnas
+     * @var array 
+     */
     private $colums;
     /**
      * @var PDO
      */
     private $conexion;
 
+    /**
+     * @param null $key
+     * @param null $tableName
+     * @param null $colums
+     */
     public function __construct($key = null, $tableName = null, $colums = null){
         $this->key = $key;
         $this->tableName = $tableName;
         $this->colums = $colums;
     }
 
+    /**
+     * @param string $host
+     * @param string $dbname
+     * @param string $user
+     * @param string $pwd
+     * 
+     * @return void
+     */
     public function setConexion($host = 'localhost', $dbname, $user, $pwd){
         try {
             $pdo = new PDO('mysql:host='.$host.';dbname='.$dbname, $user, $pwd);
@@ -25,30 +50,53 @@ class EncryptionPHP
         }
     }
 
+    /**
+     * @param string $key
+     * 
+     * @return void
+     */
     public function setKey($key){
         $this->key = $key;
     }
 
+    /**
+     * @param string $tableName
+     * 
+     * @return void
+     */
     public function setTableName($tableName){
         $this->tableName = $tableName;
     }
 
+    /**
+     * @param array $colums
+     * 
+     * @return void
+     */
     public function setColums($colums){
         $this->colums = $colums;
     }
 
+    /**
+     * @param array $values_p
+     * 
+     * @return void
+     */
     public function encryptDB($values_p){
 
         $colums = $this->concat($this->colums);
-
         $value = $this->concat($values_p, $this->key);
-        
         $sentencia = $this->conexion->prepare("CALL encrypt(\"$this->tableName\",\"$colums\",\"$value\")");
-
         $sentencia->execute();
         
     }
 
+    /**
+     * @param array $array
+     * @param null $key
+     * 
+     * @return string
+     */
     function concat($array, $key = null){
         for ($i = 0, $c = ""; $i < count($array); $i++) {
             if ($i == 0) {
